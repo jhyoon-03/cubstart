a0cad6ff9dc596aff17e06781f33f4680f5007da
Jordan <jordantyee@berkeley.edu>
2021-03-13 01:46:36 -0800

    [0313] added in empty field checker to log in screen as well by Jordan

--- march/bookMatch/bookMatch/ViewController.swift
+++ march/bookMatch/bookMatch/ViewController.swift
@@ -11,13 +11,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var usernameText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var invalidErrorMessage: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        invalidErrorMessage.isHidden = true
     }
 
+    // check if a text field is filled out or not
+    func emptyChecker(text: UITextField) -> Bool{
+        return text.text == ""
+    }
+    
     @IBAction func loginButton(_ sender: Any) {
-        performSegue(withIdentifier: "toProfile", sender: self)
+        if emptyChecker(text: usernameText) || emptyChecker(text: passwordText) {
+            invalidErrorMessage.isHidden = false
+        }
+        else {
+            invalidErrorMessage.isHidden = true
+            performSegue(withIdentifier: "toProfile", sender: self)
+        }
     }
     
     @IBAction func signUpButton(_ sender: Any) {
