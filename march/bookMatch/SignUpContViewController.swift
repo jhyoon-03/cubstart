04a8859d2288189737a88d06889ff8beaa22c7d2
Haroun Khaleel <harounkhaleel@berkeley.edu>
2021-03-16 18:25:44 -0700

    added functions and error messages to verify that password is >= 8 characters and contains special characters

--- march/bookMatch/SignUpContViewController.swift
+++ march/bookMatch/SignUpContViewController.swift
@@ -16,9 +16,23 @@ class SignUpContViewController: UIViewController {
     @IBOutlet weak var confirmPasswordText: UITextField!
     @IBOutlet weak var emptyFieldErrorMessage: UILabel!
     @IBOutlet weak var passwordConfirmationErrorMessage: UILabel!
+    @IBOutlet weak var passwordSpecialCharacterMessage: UILabel!
+    @IBOutlet weak var passwordLengthMessage: UILabel!
     
     // store profile information
     static var location: String = ""
+    
+    // returns whether the string is a valid password containing special characters
+    func isValidPasswordCharacters(_ password: String) -> Bool {
+        let specialCharacters = ".*[^A-Za-z0-9].*"
+        let testPassword = NSPredicate(format:"SELF MATCHES %@", specialCharacters)
+        return testPassword.evaluate(with: password)
+    }
+    
+    //returns whether the string is a valid password at least 8 characters
+    func isValidPasswordLength(_ password: String) -> Bool {
+        return password.count >= 8
+    }
     
     // check if a text field is filled out or not
     func emptyChecker(text: UITextField) -> Bool{
@@ -38,15 +52,34 @@ class SignUpContViewController: UIViewController {
         if emptyVarCheckArray.contains(true) {
             emptyFieldErrorMessage.isHidden = false
         }
+        // checks if password length is valid
+        else if !isValidPasswordLength(passwordText.text!){
+            passwordLengthMessage.isHidden = false
+            emptyFieldErrorMessage.isHidden = true
+            passwordSpecialCharacterMessage.isHidden = true
+            passwordConfirmationErrorMessage.isHidden = true
+
+        }
+        // checks if password characters are valid
+        else if !isValidPasswordCharacters(passwordText.text!) {
+            passwordSpecialCharacterMessage.isHidden = false
+            passwordLengthMessage.isHidden = true
+            emptyFieldErrorMessage.isHidden = true
+            passwordConfirmationErrorMessage.isHidden = true
+        }
         // checks if the password and confirmation password are the same
         else if passwordText.text != confirmPasswordText.text {
             passwordConfirmationErrorMessage.isHidden = false
             emptyFieldErrorMessage.isHidden = true
+            passwordSpecialCharacterMessage.isHidden = true
+            passwordLengthMessage.isHidden = true
         }
         else {
             SignUpContViewController.location = cityText.text! + ", " + stateText.text!
             emptyFieldErrorMessage.isHidden = true
             passwordConfirmationErrorMessage.isHidden = true
+            passwordSpecialCharacterMessage.isHidden = true
+            passwordLengthMessage.isHidden = true
             performSegue(withIdentifier: "toEmailConfirm", sender: self)
         }
     }
