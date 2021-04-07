68adea810e3900637ccf85d124c3812c0f62771d
Haroun Khaleel <harounkhaleel@berkeley.edu>
2021-04-07 15:43:27 -0700

    Created TableViewCell for products

--- march/bookMatch/ShopViewController.swift
+++ march/bookMatch/ShopViewController.swift
@@ -7,6 +7,25 @@
 
 import UIKit
 
+//Each product retrieved from the database is displayed in a ShopTableViewCell in the ShopViewController
+class ShopTableViewCell: UITableViewCell {
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    @IBOutlet weak var isbnLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    //When the user taps on the cell, the application will take them to the product page for that product
+    @IBAction func selectProduct(_ sender: Any) {
+    }
+    
+}
+
+//The view controller for the show page of the application
 class ShopViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,7 +34,8 @@ class ShopViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var shopSearchBar: UISearchBar!
+    
     /*
     // MARK: - Navigation
 
